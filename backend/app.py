44d62b888cf28c55c740af2127b6b3146c8f3e9a
chemdocsweb <lokeshvarma.117@gmail.com>
2024-06-30 18:03:58 +0530

    update local folder to remote

---
+++ backend/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, send_from_directory
+import os
+from dataFetchFile import textbooksData
+
+textbooks_link = "https://docs.google.com/spreadsheets/d/1xI19BIX49dks6SqWn1H1IMq07ppVvrIS/export?format=csv"
+
+file_path = "../frontend/public"
+app = Flask(__name__, template_folder=file_path, static_url_path='/static', static_folder=file_path)
+
+'''@app.route('/')
+def index():
+    return render_template('index.html')'''
+
+@app.route('/static/<path:filename>')
+def static_files(filename):
+    return send_from_directory(file_path, filename)
+
+@app.route('/')
+def textbooks():
+    subs_count, subjects_info, textbooks = textbooksData(textbooks_link)
+    
+    return render_template("textbooks.html", subs_count = subs_count, subs_info = subjects_info, textbooks = textbooks)
+    
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))  # Use the PORT environment variable or default to 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
---
+++ backend/dataFetchFile.py
@@ -0,0 +1,21 @@
+def textbooksData(link):
+  import requests as rq
+  import pandas as pd
+  from io import StringIO
+  
+  '''response = rq.get(link)
+  response.raise_for_status()
+
+  csv_data = StringIO(response.text)'''
+  csv = "textbooks.csv"
+  textbooks = pd.read_csv(csv)
+  
+  subjects_info = textbooks[['subject', 'sub_info']].drop_duplicates()
+  subs_count = list(range(1, len(subjects_info['subject']) + 1))
+  
+  subjects_info = subjects_info.to_dict("records")
+  textbooks = textbooks.to_dict("records")
+  
+  return subs_count, subjects_info, textbooks
+
+# subs_count, subjects_info, textbooks = textbooksData("https://docs.google.com/spreadsheets/d/1xI19BIX49dks6SqWn1H1IMq07ppVvrIS/export?format=csv")
